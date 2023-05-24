8143b9e9c8d127ac0302c6cd1a41bacdefe1dbc7
Lisa <125468847+lisaASM@users.noreply.github.com>
2023-05-24 15:13:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import psycopg2
 
 app = Flask(__name__)
-
-conn = psycopg2.connect(user="postgres",
-                        database="service_db",
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
                         password="deadmike07",
                         host="localhost",
                         port="5432")
@@ -16,15 +15,20 @@ cursor = conn.cursor()
 def index():
     return render_template('login.html')
 
-
-@app.route('/', methods=['POST'])
-def login():
-    username = request.form.get('login')
+@app.route('/login/', methods=['POST'])
+def login_post():
+    username = request.form.get('username')
     password = request.form.get('password')
-    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(login), str(password)))
+    if not username:
+        return render_template('login.html', error='Введите логин')
+    if not password:
+        return render_template('login.html', error='Введите пароль')
+    cursor.execute("SELECT * FROM service.users WHERE username=%s AND password=%s", (str(username), str(password)))
     records = list(cursor.fetchall())
-
     if records == []:
-        return render_template("account.html")
+        return render_template('account.html')
+    return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
 
-    return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+if __name__ == '__main__':
+    app.run(debug=True)
+
